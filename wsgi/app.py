ec7a6b53168760113ddb0d670cf652a9a0b7cf3f
Gonzalo Rivero <griverorz@gmail.com>
2017-03-27 14:43:06 -0400

    Provides endpoints from state to bg

--- wsgi/app.py
+++ wsgi/app.py
@@ -2,6 +2,7 @@
 
 from flask import (
     Flask,
+    jsonify,
     make_response)
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
@@ -9,7 +10,8 @@ from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gonzalorivero@localhost:5432/pdb'
+URI = 'postgresql://gonzalorivero@localhost:5432/pdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = URI
 db = SQLAlchemy(app)
 
 
@@ -17,6 +19,7 @@ class Pdb(db.Model):
     GIDBG = db.Column(db.Integer, primary_key=True)
     State = db.Column(db.Integer)
     State_name = db.Column(db.String(60))
+    County = db.Column(db.Integer)
     County_name = db.Column(db.String(120))
     Tract = db.Column(db.Integer)
     Block_Group = db.Column(db.Integer)
@@ -32,12 +35,43 @@ class Pdb(db.Model):
 class PdbBlockgroup(Resource):
     def __init__(self):
         self.headers = {'Content-Type': 'text/html'}
-        
-    def get(self, bgid):
-        lrs = Pdb.query.filter_by(username=bgid).first_or_404()
-        return(make_response(lrs))
 
-api.add_resource(PdbBlockgroup, '/minipdb/blockgroup')
+    def get(self, state, county=None, tract=None, blockgroup=None):
+        queries = [Pdb.State == state]
+        if county is not None:
+            queries.append(Pdb.County == county)
+            if tract is not None:
+                queries.append(Pdb.Tract == tract)
+                if blockgroup is not None:
+                    queries.append(Pdb.Block_Group == blockgroup)
+        lrs = (Pdb.query.
+               filter(*queries).
+               all())
+        try:
+            datalist = []
+            for i in lrs:
+                data = {"state": i.State_name,
+                        "county": i.County_name,
+                        "tract": i.Tract,
+                        "blockgroup": i.Block_Group,
+                        "score": i.Low_Response_Score}
+                datalist.append(data)
+            res = {"data": datalist,
+                   "response": 200}
+        except:
+            res = {"error": {"message": "Not found"},
+                   "response": 404}
+        return(jsonify(res))
+
+
+api.add_resource(
+    PdbBlockgroup,
+    '/minipdb',
+    '/minipdb/<state>/',
+    '/minipdb/<state>/<county>/',
+    '/minipdb/<state>/<county>/<tract>/',
+    '/minipdb/<state>/<county>/<tract>/<blockgroup>'
+)
 
 if __name__ == '__main__':
     app.run(debug=True)
