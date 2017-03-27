778648ab99a454909f55d3c7ab721ce772ef28e5
Gonzalo Rivero <griverorz@gmail.com>
2017-03-27 16:54:34 -0400

    Requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
+
 setup(name="minipdb",
       version="0.1",
       description="Small API to expose the Low Response Score from Census",
       author="Gonzalo Rivero",
       author_email="gonzalorivero@westat.com",
-      install_requires=[
-          'Flask==0.10.1',
-          'Flask-SQLAlchemy==1.0'
-      ],
-)
+      install_requires=reqs)
