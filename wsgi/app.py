f62259f92c2f141406dff1d349b11c50a4a63c48
Gonzalo Rivero <griverorz@gmail.com>
2017-03-28 20:08:36 -0400

    Make POST input

--- wsgi/app.py
+++ wsgi/app.py
@@ -2,11 +2,41 @@
 
 from flask import (
     Flask,
-    jsonify)
+    request)
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
 from datetime import datetime
 from config import Config, Prod
+from marshmallow import (
+    Schema,
+    fields,
+    post_load,
+    validates_schema,
+    ValidationError)
+
+
+class RequestSchema(Schema):
+    state = fields.Integer(required=True,
+                           error_messages={'required': "State is required"})
+    county = fields.Integer()
+    tract = fields.Integer()
+    blockgroup = fields.Integer()
+
+    @post_load
+    def make_object(self, data):
+        if "blockgroup" in data:
+            if "tract" not in data:
+                raise ValidationError("Schema not nested")
+        if "tract" in data:
+            if "county" not in data:
+                raise ValidationError("Schema not nested")
+        return(data)
+
+    @validates_schema(pass_original=True)
+    def check_unknown_fields(self, data, original_data):
+        unknown = set(original_data) - set(self.fields)
+        if unknown:
+            raise ValidationError('Unknown field', unknown)
 
 
 app = Flask(__name__)
@@ -39,54 +69,69 @@ class Pdb(db.Model):
         return '<Pdb %r>' % self.GIDBG
 
 
+def build_query(state, county=None, tract=None, blockgroup=None):
+    queries = [Pdb.State == state]
+    if county is not None:
+        queries.append(Pdb.County == county)
+        if tract is not None:
+            queries.append(Pdb.Tract == tract)
+            if blockgroup is not None:
+                queries.append(Pdb.Block_Group == blockgroup)
+    res = Pdb.query.filter(*queries).all()
+    return(res)
+
+
+def parse_results(data):
+    res = [{"state": i.State_name,
+            "state_id": i.State,
+            "county": i.County_name,
+            "county_id": i.County,
+            "tract": i.Tract,
+            "blockgroup": i.Block_Group,
+            "score": i.Low_Response_Score/100 if i.Low_Response_Score else None}
+           for i in data]
+    return(res)
+        
+
+def process_query(data):
+    lrs = build_query(**data)
+    if lrs:
+        query_res = parse_results(lrs)
+        res = {"data": query_res,
+               "version": "v2015-07-28",
+               "timestamp": str(datetime.now())}
+        statuscode = 200
+    else:
+        res = {"error":
+               {"message": "Resource {} not found".format(data)},
+               "timestamp": str(datetime.now())}
+        statuscode = 404
+    return(res, statuscode)
+
+
 class PdbLRS(Resource):
     def __init__(self):
         self.headers = {'Content-Type': 'text/html'}
 
-    def get(self, state, county=None, tract=None, blockgroup=None):
-        queries = [Pdb.State == state]
-        if county is not None:
-            queries.append(Pdb.County == county)
-            if tract is not None:
-                queries.append(Pdb.Tract == tract)
-                if blockgroup is not None:
-                    queries.append(Pdb.Block_Group == blockgroup)
-        lrs = (Pdb.query.
-               filter(*queries).
-               all())
-        if lrs:
-            datalist = []
-            for i in lrs:
-                score = i.Low_Response_Score/100 if i.Low_Response_Score else None
-                data = {"state": i.State_name,
-                        "state_id": i.State,
-                        "county": i.County_name,
-                        "county_id": i.County,
-                        "tract": i.Tract,
-                        "blockgroup": i.Block_Group,
-                        "score": score}
-                datalist.append(data)
-            res = {"data": datalist,
-                   "response": 200,
-                   "version": "v2015-07-28",
-                   "timestamp": datetime.now()}
-        else:
-            attempt = {'state': state,
-                       'county': county,
-                       'tract': tract,
-                       'blockgroup': blockgroup}
-            res = {"error": {"message": "Resource {} not found".format(attempt)},
-                   "response": 404,
-                   "timestamp": datetime.now()}
-        return(jsonify(res))
+    def post(self):
+        try:
+            inputdata = request.get_json()
+        except:
+            res = {"error": {"message": "Malformed request"},
+                   "timestamp": str(datetime.now())}
+            statuscode = 400
+            return(res, statuscode)
+        try:
+            data = RequestSchema(strict=True).load(inputdata).data
+        except ValidationError as e:
+            res = {"error": {"message": str(e)},
+                   "timestamp": str(datetime.now())}
+            statuscode = 400
+            return(res, statuscode)
+        res, statuscode = process_query(data)
+        return(res, statuscode)
 
-
-api.add_resource(
-    PdbLRS,
-    '/minipdb/<state>/',
-    '/minipdb/<state>/<county>/',
-    '/minipdb/<state>/<county>/<tract>/',
-    '/minipdb/<state>/<county>/<tract>/<blockgroup>')
+api.add_resource(PdbLRS, '/minipdb')
 
 
 if __name__ == '__main__':
